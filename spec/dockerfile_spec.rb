cfcc7906206a49c3e4c71cea5d7f8a407d00a8a8
Edwin van der Graaf <edwin@digitpaint.nl>
2015-10-21 12:04:57 +0200

    Implement image building on circle ci
    * Add some debug information to follow the progress
    * Overwrite the docker container `method` remove since this isnt
      supported on circleci
    * Downgrade the dependencies of specinfra since this gem moved to a new
      exec function of docker which is also not supported on circleci (LXC)
    
    A successfull build on master is automatically pushed to docker hub

--- spec/dockerfile_spec.rb
+++ spec/dockerfile_spec.rb
@@ -1,9 +1,22 @@
 require "serverspec"
 require "docker"
 
+# Workaround needed for circleCI
+if ENV["CIRCLECI"]
+  class Docker::Container
+    def remove(*); end
+    alias_method :delete, :remove
+  end
+end
+
 describe "Dockerfile" do
   before(:all) do
-    image = Docker::Image.build_from_dir(".")
+    image = Docker::Image.build_from_dir(".") do |v|
+      matches = v.match(/{\"stream\":\"(Step[^\\"]*)/)
+      if matches
+        puts "=> #{matches.captures[0]}"
+      end
+    end
 
     set :os, family: :debian
     set :backend, :docker
